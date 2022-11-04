681554bbc7b3fd0c3bf8072b2b579d71d65730d6
mthskzk <116724950+mthskzk@users.noreply.github.com>
2022-11-04 12:15:35 +0000

    [Start]2_9

---
+++ new.rb
@@ -0,0 +1,31 @@
+class Car
+  def move(direction,distance)
+    self.turn(direction)
+    self.run(distance)
+  end
+
+  def run(distance)
+    puts "車で#{distance}キロ走ります。"
+  end
+
+  def turn(direction)
+    puts "#{direction}に曲がります。"
+  end
+end
+
+car=Car.new
+car.move("左",3)
+
+car.turn("右")
+car.run(5)
+
+# class名のはじめは大文字にする
+# car=Car.new でインスタンスを作成する
+
+class Car
+  def self.run(distance)
+    puts "車で#{distance}キロ走ります。"
+  end
+end
+
+Car.run(10)
